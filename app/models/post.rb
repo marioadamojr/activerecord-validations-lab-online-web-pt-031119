8291833939f60b8ffc6c5cd829734c0a8966a082
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-10 15:43:13 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :non_clickbait
 
-  private
+  
 
   clickbait_words = [
     /Won't Believe/i,

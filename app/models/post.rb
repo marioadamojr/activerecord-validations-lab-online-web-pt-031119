337bdd5d5d25678ae1f8e89faafc68dfaaaf19fa
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-10 15:45:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :non_clickbait
+
+
+
+  CLICKBAIT_WORDS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def non_clickbait
+    if CLICKBAIT_WORDS.none? { |pat| pat.match title}
+      errors.add(:title, "No Clickbait Allowed")
+    end
+  end
 end

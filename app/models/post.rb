cbdba71035cbc427c1bb4adbbd657b6a17f63447
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-10 15:38:36 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,12 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  # validate :non_clickbait
+  validate :non_clickbait
 
-  # private
-  # def non_clickbait
-  #   if self.title.include?("Won't Believe" || "Secret" || "Top [0..100]" || "Guess")
-  #     errors.add(:title, "No Clickbait Allowed")
-  #   end
-  # end
+  private
+  def non_clickbait
+    if self.title.include?("Won't Believe" || "Secret" || "Top [0..100]" || "Guess")
+      errors.add(:title, "No Clickbait Allowed")
+    end
+  end
 end
